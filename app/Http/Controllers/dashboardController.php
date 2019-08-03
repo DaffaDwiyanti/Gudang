bbd8d59b6961eb270b78bfa9f9cb28509a19c841
muhammad refda <muhammadrefda@gmail.com>
2019-08-03 21:11:48 +0700

    tambahin chart

--- app/Http/Controllers/dashboardController.php
+++ app/Http/Controllers/dashboardController.php
@@ -7,167 +7,25 @@ use App\Http\Requests\UpdatebarangRequest;
 use App\Repositories\barangRepository;
 use App\Http\Controllers\AppBaseController;
 use Illuminate\Http\Request;
+use DB;
 use Flash;
 use Response;
 
 class dashboardController extends AppBaseController
 {
-    /** @var  barangRepository */
-
-
-    private $barangRepository;
-
-    public function __construct(barangRepository $barangRepo)
+    function index()
     {
-        $this->barangRepository = $barangRepo;
+        $data = DB::table('barangs')
+            ->select(
+                DB::raw('namaBarang as namaBarang'),
+                DB::raw('count(*) as number')
+            )
+            ->groupBy('namaBarang')
+            ->get();
+        $array[] = ['namaBarang', 'Number'];
+        foreach ($data as $key => $value) {
+            $array[++$key] = [$value->namaBarang, $value->number];
+        }
+        return view('dashboard')->with('status', json_encode($array));
     }
-
-    // public function __construct(barangRepository $barangRepo)
-    // {
-    //     $this->barangRepository = $barangRepo;
-    // }
-
-    /**
-     * Display a listing of the barang.
-     *
-     * @param Request $request
-     *
-     * @return Response
-     */
-    public function index(Request $request)
-    {
-        // $barangs = $this->barangRepository->all();
-
-        return view('dashboard');
-    }
-
-    /**
-     * Show the form for creating a new barang.
-     *
-     * @return Response
-     */
-    public function create()
-    { }
-
-    /**
-     * Store a newly created barang in storage.
-     *
-     * @param CreatebarangRequest $request
-     *
-     * @return Response
-     */
-    public function store(CreatebarangRequest $request)
-    {
-        // $input = $request->all();
-
-        // $barang = $this->barangRepository->create($input);
-
-        // Flash::success('Barang saved successfully.');
-
-        // return redirect(route('barangs.index'));
-    }
-
-    /**
-     * Display the specified barang.
-     *
-     * @param int $id
-     *
-     * @return Response
-     */
-    public function show($id)
-    {
-        // $barang = $this->barangRepository->find($id);
-
-        // if (empty($barang)) {
-        //     Flash::error('Barang not found');
-
-        //     return redirect(route('barangs.index'));
-        // }
-
-        // return view('barangs.show')->with('barang', $barang);
-    }
-
-    /**
-     * Show the form for editing the specified barang.
-     *
-     * @param int $id
-     *
-     * @return Response
-     */
-    public function edit($id)
-    {
-        // $barang = $this->barangRepository->find($id);
-
-        // if (empty($barang)) {
-        //     Flash::error('Barang not found');
-
-        //     return redirect(route('barangs.index'));
-        // }
-
-        // return view('barangs.edit')->with('barang', $barang);
-    }
-
-    /**
-     * Update the specified barang in storage.
-     *
-     * @param int $id
-     * @param UpdatebarangRequest $request
-     *
-     * @return Response
-     */
-    public function update($id, UpdatebarangRequest $request)
-    {
-        // $barang = $this->barangRepository->find($id);
-
-        // if (empty($barang)) {
-        //     Flash::error('Barang not found');
-
-        //     return redirect(route('barangs.index'));
-        // }
-
-        // $barang = $this->barangRepository->update($request->all(), $id);
-
-        // Flash::success('Barang updated successfully.');
-
-        // return redirect(route('barangs.index'));
-    }
-
-    /**
-     * Remove the specified barang from storage.
-     *
-     * @param int $id
-     *
-     * @throws \Exception
-     *
-     * @return Response
-     */
-    public function destroy($id)
-    {
-        // $barang = $this->barangRepository->find($id);
-
-        // if (empty($barang)) {
-        //     Flash::error('Barang not found');
-
-        //     return redirect(route('barangs.index'));
-        // }
-
-        // $this->barangRepository->delete($id);
-
-        // Flash::success('Barang deleted successfully.');
-
-        // return redirect(route('barangs.index'));
-    }
-
-    // public function validasiTarget($id, Request $request){
-
-    //     $valTarget = $this->barangRepository->
-
-    //     if(){}
-    //         else {
-    //             # code...
-    //         }
-
-    // }
-
-
 }
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,23 +1,56 @@
 @extends('layouts.app')
-
 @section('content')
+
+<head>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
+    <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />
+    <style type="text/css">
+        .box {
+            width: 800px;
+            margin: 0 auto;
+        }
+    </style>
+    <script type="text/javascript">
+        var analytics = <?php echo $status; ?>
+
+        google.charts.load('current', {
+            'packages': ['corechart']
+        });
+
+        google.charts.setOnLoadCallback(drawChart);
+
+        function drawChart() {
+            var data = google.visualization.arrayToDataTable(analytics);
+
+            var chart = new google.visualization.PieChart(document.getElementById('pie_chart'));
+            chart.draw(data);
+        }
+    </script>
+</head>
 
 <section class="content-header">
     <h1 class="pull-left">Dashboard</h1>
-    <!-- <h1 class="pull-right">
-           <a class="btn btn-primary pull-right" style="margin-top: -10px;margin-bottom: 5px" href="{!! route('orders.create') !!}">Add New</a>
-        </h1> -->
 </section>
 <div class="content">
     <div class="clearfix"></div>
 
     @include('flash::message')
+    <div class="container">
+        <h3 align="center">Jumlah paket yang telah diterima maupun sedang di proses</h3><br />
+
+        <div class="panel panel-default">
+            <div class="panel-body" align="center">
+                <div id="pie_chart" style="width:750px; height:450px;">
+
+                </div>
+            </div>
+        </div>
+
+    </div>
 
     <div class="clearfix"></div>
     <div class="box box-primary">
-        <!-- <div class="box-body">
-                    @include('orders.table')
-            </div> -->
     </div>
     <div class="text-center">
 
